826375f344bf8580a3975a9258084ea75d52720f
Victor de la Fouchardière <victor.dlf@outlook.fr>
2017-10-19 15:11:17 +0200

    add js & scss minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,48 @@
-/*----------  Récupération des modules  ----------*/
+const gulp = require('gulp'),
+compass = require('gulp-compass'),
+autoprefixer = require('gulp-autoprefixer'),
+rename = require('gulp-rename'),
+cleanCSS = require('gulp-clean-css'),
+uglify = require('gulp-uglify'),
+sourcemaps = require('gulp-sourcemaps'),
+babel = require('gulp-babel'),
+concat = require('gulp-concat'),
+print = require('gulp-print'),
+runSequence = require('run-sequence'),
+fs = require('fs');
 
 // Set true if you're in production
-const inProduction = true;
+const inProduction = false;
 
-// Put your script library name here
-var lib = [];
+runSequence.options.ignoreUndefinedTasks = true;
 
-const gulp = require('gulp'),
-	compass = require('gulp-compass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	rename = require('gulp-rename'),
-	cleanCSS = require('gulp-clean-css'),
-	uglify = require('gulp-uglify'),
-	sourcemaps = require('gulp-sourcemaps'),
-	babel = require('gulp-babel'),
-	concat = require('gulp-concat'),
-	print = require('gulp-print'),
-	runSequence = require('run-sequence'),
-	imagemin = require('gulp-imagemin');
+// Set your pathfiles
+var assetsPath = '',
+scssFilename = 'styles.scss',
+jsPath = assetsPath+'js',
+cssPath = assetsPath+'css',
+scssPath = assetsPath+'scss',
+imgPath = assetsPath+'img';
+
+const pathLibJs = ['lib'];
+const scriptFile = 'script.min.js';
+
+var libJs = ['turbolinks'];
 
 /*----------  Styles  ----------*/
 
-// Compass task
-gulp.task('styles', function() {
-	return gulp.src(['assets/scss/*.scss'])
+fs.access(assetsPath+scssPath+'/'+scssFilename, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+	if (err) {
+		fs.writeFile(scssPath+'/'+scssFilename, '@import "compass";', 'utf8');
+	}
+	runSequence('css');
+});
+
+gulp.task('css', function() {
+	return gulp.src([scssPath+'/*.scss'])
 		.pipe(compass({
-		 	css: 'assets/css',
-			sass: 'assets/scss',
-			config_file: './config.rb',
+		 	css: cssPath,
+			sass: scssPath,
 		}))
 		.on('error', function(error) {
 	      console.log(error);
@@ -37,26 +52,17 @@ gulp.task('styles', function() {
 	      return "file created : " + filepath;
 	    }))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    	.pipe(gulp.dest('assets/css'))
+    	.pipe(gulp.dest(cssPath))
     	.pipe(rename({ suffix: '.min' }))
     	.pipe(cleanCSS({compatibility: 'ie8'}))
-    	.pipe(gulp.dest('assets/css'));
+    	.pipe(gulp.dest(cssPath));
 });
-
-/*----------  Images  ----------*/	
-
-// // Compressed task
-// gulp.task('image', function() {
-// 	return gulp.src(['public/assets/img/*'])
-// 		.pipe(imagemin())
-// 		.pipe(gulp.dest('public/assets/imgComp'));
-// });
 
 /*----------  Scripts  ----------*/
 
 if (!inProduction) {
 	gulp.task('myJs', function() {
-		return gulp.src('assets/js/*.js')
+		return gulp.src(assetsPath+jsPath+'/*.js')
 			.pipe(sourcemaps.init())
 	 		.pipe(babel({
 	            presets: ['es2015']
@@ -65,7 +71,7 @@ if (!inProduction) {
 		         console.log(e);
 		    }))
 			.pipe(rename({ suffix: '.min' }))
-			.pipe(gulp.dest('assets/js/min'))
+			.pipe(gulp.dest(assetsPath+jsPath+'/min'))
 
 			.pipe(print(function(filepath) {
 		      return "file created : " + filepath;
@@ -75,10 +81,10 @@ if (!inProduction) {
 
 else {
 	gulp.task('myJs', function() {
-		return gulp.src('assets/js/*.js')
+		return gulp.src(assetsPath+jsPath+'/*.js')
 			.pipe(sourcemaps.init())
 			.pipe(rename({ suffix: '.min' }))
-			.pipe(gulp.dest('assets/js/min'))
+			.pipe(gulp.dest(assetsPath+jsPath+'/min'))
 
 			.pipe(print(function(filepath) {
 		      return "file created : " + filepath;
@@ -86,30 +92,37 @@ else {
 	});
 }
 
-var scripts = [];
-for (var i = 0; i < lib.length; i++) {
-	scripts.push('lib/'+lib[i]+'/*.js');
+gulp.task('default', ['scripts']);
+
+
+var scripts = [], x;
+for (var i = 0; i < libJs.length; i++) {
+	x = libJs[i];
+	fs.access(pathLibJs+'/'+libJs[i], fs.constants.R_OK | fs.constants.W_OK, (err) => {
+		if (err) {
+			console.warn('Directory doesn\'t exist '+err.path);
+		} else {
+			scripts.push(pathLibJs+'/'+x+'/*.js');
+		}
+	});
 }
 
-scripts.push('assets/js/min/*.js');
+scripts.push(assetsPath+jsPath+'/min/*.js');
 
-gulp.task('concat', function() {
+gulp.task('concat__JS', function() {
   gulp.src(scripts)
-    .pipe(concat('script.min.js'))
-    .pipe(gulp.dest('assets/js/min'));
+    .pipe(concat(scriptFile))
+    .pipe(gulp.dest(assetsPath+jsPath+'/min'));
 });
 
 gulp.task('scripts', function() {
-	runSequence('myJs','concat');
+	runSequence('myJs','concat__JS');
 });
 
+// /*----------  Live  ----------*/
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
-
-/*----------  Live  ----------*/
-
-// Watch task
-gulp.task('watch', function() {
-	gulp.watch('./assets/scss/*.scss', ['styles']);
-	gulp.watch('./assets/js/*.js', ['scripts']);
-});
+// // Watch task
+// gulp.task('watch', function() {
+// 	gulp.watch('./assets/scss/*.scss', ['styles']);
+// 	gulp.watch('./assets/js/*.js', ['scripts']);
+// });
